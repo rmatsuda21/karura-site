41420cb3a10e3f87e45e50c4ac87fcc3bfa2ff3d
rmatsuda21 <reo.matsuda@gmail.com>
2023-11-15 18:28:28 -0600

    update department

---
+++ js/support.js
@@ -0,0 +1,10 @@
+const supportBtn = document.querySelector("#supportBtn");
+
+supportBtn.addEventListener("click", () => {
+  showModal(
+    `<div class="support">
+      <h1>Support</h1>
+      <p>Send us an email at <a>something@gmail.com</a></p>
+    </div>`
+  );
+});
