a0e293ca065e3d5f0633f3480e045405fb50ac09
rmatsuda21 <reo.matsuda@gmail.com>
2023-11-22 20:33:44 -0600

    fix support modal

--- js/support.js
+++ js/support.js
@@ -7,11 +7,11 @@ supportBtn.addEventListener("click", () => {
       <h1>Thank you for<br /> visiting our website!</h1>
       <h2>We appreciate your continued support</h2>
       <div class="info">
+        <span><strong>Branch Number</strong>426</span>
         <span><strong>Account Number</strong>3052264</span>
-        <span><strong>Account Number</strong>3052264</span>
-        <span><strong>Account Number</strong>3052264</span>
-        <span><strong>Account Number</strong>3052264</span>
-        <span><strong>Account Number</strong>3052264</span>
+        <span><strong>Branch Name</strong>Nagano</span>
+        <span><strong>Account Name</strong>KARURA</span>
+        <span><strong>Type</strong>Saving Account</span>
       </div>
       <a href="https://gofund.me/99657e2a" class="special">Support us here</a>
     </div>`
