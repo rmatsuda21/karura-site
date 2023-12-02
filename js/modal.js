a9fb827468d85c4e9473434541c182ebc2db5886
rmatsuda21 <reo.matsuda@gmail.com>
2023-12-02 13:44:24 -0800

    fix news fetch

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,10 @@
-window.onload = () => {
+addEventListener("load", () => {
   const modal = document.getElementById("modal");
   modal.addEventListener("click", hideModal);
-};
+});
 
 const hideModal = (e) => {
+  console.log(e.target);
   if (e.target.id !== "modal") return;
 
   const modal = document.getElementById("modal");
